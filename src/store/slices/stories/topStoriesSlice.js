73c1bbb8cc505bec44a5aa352b5a0ae86ec75aa8
makspooh <maksimpooh@gmail.com>
2023-10-14 17:01:35 +0300

    @redux/toolkit added

---
+++ src/store/slices/stories/topStoriesSlice.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-param-reassign */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+import initTopStories from "../../services/stories";
+
+const storiesSlice = createSlice({
+  name: "stories",
+  initialState: { stories: null, isLoading: true, error: null },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(initTopStories.pending, (state) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(initTopStories.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+
+    builder.addCase(initTopStories.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    });
+  },
+});
+
+export const { setUser } = storiesSlice.actions;
+export default storiesSlice.reducer;
